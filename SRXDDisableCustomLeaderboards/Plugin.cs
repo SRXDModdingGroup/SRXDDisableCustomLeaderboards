d343cde9f916a58a473b6e04eb4ed512ff016e4b
Amy Y <ctrlalternativedelete@gmail.com>
2021-12-28 00:26:42 +1100

    init

---
+++ SRXDDisableCustomLeaderboards/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SRXDDisableCustomLeaderboards
+{
+    [BepInPlugin("org.srxdmoddinggroup.disablecustomleaderboards", "disablecustomleaderboards", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Custom Leaderboards are Disabled!");
+            Harmony.CreateAndPatchAll(typeof(Patches));
+        }
+        private class Patches
+        {
+            [HarmonyPatch(typeof(TrackInfo), MethodType.Constructor)]
+            [HarmonyPatch(typeof(TrackInfoMetadata), MethodType.Constructor)]
+            [HarmonyPostfix]
+            private static void patch(ref TrackInfo __instance)
+            {
+                __instance.allowCustomLeaderboardCreation = false;
+            }
+
+            [HarmonyPatch(typeof(LeaderboardSubmissionHelper), nameof(LeaderboardSubmissionHelper.SubmitLeaderboard))]
+            [HarmonyPrefix]
+            private static bool patch1(PlayState __0)
+            {
+                return !__0.TrackInfoRef.IsCustomFile;
+            }
+        }
+    }
+}
